bb1baff427164f7813ec259b3398733369e6f68f
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-06-02 21:30:02 -0500

    Add examples for finding and replacing patterns using re.findall and re.sub

--- 05_regex/01_re.py
+++ 05_regex/01_re.py
@@ -57,3 +57,45 @@ if match:
   print("Posición de fin:", match.end())
 else:
   print("No he encontrado el patrón en el texto.")
+
+# ----------------
+# Encontrar todas las ocurrencias de un patrón en un texto
+# .findall() devuelve una lista con todas las ocurrencias del patrón en el texto
+
+text = "Regex es una herramienta poderosa. Regex es muy útil para buscar patrones en textos. Regex es genial."
+pattern = "Regex"
+
+matches = re.findall(pattern, text)
+
+print(f"He encontrado {len(matches)} ocurrencias del patrón '{pattern}' en el texto.")
+
+# ------------------
+# .finditer() devuelve un iterador con todas las ocurrencias del patrón en el texto
+matches_iter = re.finditer(pattern, text)
+
+for match in matches_iter:
+  print(f"Texto encontrado: {match.group()}")
+  print(f"Posición de inicio: {match.start()}")
+  print(f"Posición de fin: {match.end()}")
+  print("---")
+
+# ------------------
+# Modificadores (flags)
+# Los modificadores son opciones que se pueden agregar a un patrón para cambiar su comportamiento.
+
+# re.IGNORECASE: Ignora mayúsculas y minúsculas al buscar el patrón.
+text = "Todo el mundo dice que la IA nos va a quitar el trabajo. Pero la ia no es tan mala. La Ia es genial."
+pattern = "IA"
+fount_ia = re.findall(pattern, text, re.IGNORECASE)
+
+if fount_ia: print(fount_ia)
+print("---")
+
+# Reemplazar texto
+# re.sub() reemplaza todas las ocurrencias del patrón en el texto por otro texto
+text = "Regex es una herramienta poderosa. Regex es muy útil para buscar patrones en textos. Regex es genial."
+pattern = "Regex"
+replacement = "Expresiones Regulares"
+new_text = re.sub(pattern, replacement, text) # pattern, replacement, text, count=0 (cantidad de veces a reemplazar), flags=0 (modificadores)
+print("Texto original:", text)
+print("Texto modificado:", new_text)
