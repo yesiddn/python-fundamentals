6622f6d86b70b4114bbcfd2f145e61cc7e693d50
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-06-05 22:17:06 -0500

    Add examples for quantifiers in regex, demonstrating zero or more occurrences

---
+++ 05_regex/03_quantifiers.py
@@ -0,0 +1,26 @@
+###
+# 03 - Quantifiers
+# Los cuantificadores se utilizan para especificar cuántas ocurrencias de un carácter o grupo de caracteres se deben encontrar en una cadena.
+###
+
+import re
+
+# *
+# Coincide con cero o más ocurrencias del patrón anterior
+text = "aaaba"
+pattern = r"a*"  # Coincide con cero o más 'a'
+found = re.findall(pattern, text)
+print("\n1. Cero o más ocurrencias (*)")
+print("Texto:", text)
+print("Patrón:", pattern)
+print("Resultados encontrados:", found) # ['aaa', '', '', 'b', '']  # Coincide con todas las 'a' y los espacios entre ellas
+
+# Ejercicio:
+# ¿Cuántas palabras tienen de 0 a más "a" y después una "b"?
+text = "aaaba aav ab aa b"
+pattern = r"a*b"  # Coincide con cero o más 'a' seguidas de una 'b'
+found = re.findall(pattern, text)
+print("\nEjercicio: Palabras con cero o más 'a' y una 'b'")
+print("Texto:", text)
+print("Patrón:", pattern)
+print("Resultados encontrados:", found)  # ['aaab', 'aab', 'ab', 'b']  # Coincide con todas las palabras que cumplen el patrón
