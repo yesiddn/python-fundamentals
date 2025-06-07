f3e0e26e9dca16850dfdf2dcac8c13818112d3ae
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-06-06 22:01:39 -0500

    Add exercises for matching words, digits, letters, and special characters in regex

--- 05_regex/04_sets.py
+++ 05_regex/04_sets.py
@@ -27,3 +27,44 @@ print("Texto:", text)
 print("Patrón:", pattern)
 print("Vocales encontradas:", matches)  # ['o', 'a', 'u', 'o']  # Coincide con todas las vocales en el texto
 
+# Ejercicio:
+# Encontrar las palabras man, fan y ban
+text = "man ran fan ban can omniman"
+pattern = r"\b[mbf]an\b"  # Coincide con palabras que empiezan con m, b o f y terminan con an
+matches = re.findall(pattern, text)
+print("\nEjercicio: Palabras que empiezan con m, b o f y terminan con an")
+print("Texto:", text)
+print("Patrón:", pattern)
+print("Palabras encontradas:", matches)  # ['man', 'fan', 'ban']
+
+# Rango de numeros
+text = "abc ABC 123 !@#"
+pattern = r"[0-9]"  # Coincide con cualquier dígito del 0 al 9
+matches = re.findall(pattern, text)
+print("\nEjercicio: Dígitos en un texto")
+print("Texto:", text)
+print("Patrón:", pattern)
+print("Dígitos encontrados:", matches)  # ['1', '2', '3']
+
+# Rango de letras
+pattern = r"[a-zA-Z]"  # Coincide con cualquier letra mayúscula o minúscula
+matches = re.findall(pattern, text)
+print("\nEjercicio: Letras en un texto")
+print("Texto:", text)
+print("Patrón:", pattern)
+print("Letras encontradas:", matches)  # ['a', 'b', 'c', 'A', 'B', 'C']
+
+# Rango de caracteres especiales
+pattern = r"[^a-zA-Z0-9]"  # Coincide con cualquier carácter que no sea una letra o un dígito
+matches = re.findall(pattern, text)
+print("\nEjercicio: Caracteres especiales en un texto")
+print("Texto:", text)
+print("Patrón:", pattern)
+print("Caracteres especiales encontrados:", matches)  # ['$', '_', ' ', '!', '@', '#'] 
+
+# Ejercicio final con todo lo aprendido
+# Mejorar esto: https://www.computerhope.com/jargon/r/regular-expression.png
+
+## Buscar corner cases que no pasa y arreglarlo:
+"lo.que+sea@shopping.online"
+"michael@gov.co.uk"
