a355f9c31593c53e756b4b18882597bf46e4d79d
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-06-09 21:11:40 -0500

    Add example for calculating date differences in Python

--- 06_request_ai_dates/01_dates.py
+++ 06_request_ai_dates/01_dates.py
@@ -14,3 +14,29 @@ print("Fecha y hora específica:", fecha_especifica)
 # strftime() se le pasa el objeto datetime y un formato (ver documentación)
 format_date = fecha_actual.strftime("%d/%m/%Y")
 print("Fecha actual formateada:", format_date)
+
+# Operaciones con fechas (sumar/restar días, minutos, etc.)
+yesterday = datetime.now() - timedelta(days=1)
+print("Ayer fue:", yesterday.strftime("%d/%m/%Y"))
+
+tomorrow = datetime.now() + timedelta(days=1)
+print("Mañana será:", tomorrow.strftime("%d/%m/%Y"))
+
+# Obtener componentes individuales de una fecha
+year = fecha_actual.year
+month = fecha_actual.month
+day = fecha_actual.day
+hour = fecha_actual.hour
+minute = fecha_actual.minute
+
+print("Año:", year)
+print("Mes:", month)
+print("Día:", day)
+print("Hora:", hour)
+print("Minuto:", minute)
+
+# Calcular la diferencia entre dos fechas
+date_one = datetime.now()
+date_two = datetime(2004, 12, 23)
+diferencia = date_one - date_two
+print("Diferencia entre fechas:", diferencia)
